1418e5ef9599a0987e042c7dce3c8c0f8df40057
TheGreatDevourer <anthonygould2304@outlook.com>
2022-08-11 00:31:07 -0400

    Added functionality

--- src/pages/DisasterCenters.vue
+++ src/pages/DisasterCenters.vue
@@ -22,7 +22,7 @@
                   > </q-input> 
                 </div>
       </div>
-                <q-table  :rows ="rows" :columns="columns" :filter="filter"> 
+                <q-table  :rows ="centers" :columns="columns" :filter="filter"> 
                   <template v-slot:body="props"> 
                     
                     <q-tr :props="props">
@@ -31,7 +31,7 @@
                     </q-td>
 
                       <q-td key ="address" :props="props"> 
-                        {{ props.row.address}}
+                        {{ props.row.line_1 + ' ' + props.row.line_2}}
                       </q-td>
 
                     <q-td key ="email" :props="props"> 
@@ -75,10 +75,10 @@
                     </div>
                      <div class="row">
                         <div class= "col-12 text-p q-px-xl textContent">
-                            <q-form @submit="createReliefCentre"> 
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="FName" v-model="Name"  maxlength="60" autogrow   :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 1" v-model="addressLone"  maxlength="60" autogrow  :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 2" v-model="addressLtwo"  maxlength="60" autogrow  :dense="dense" />
+                            <q-form> 
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="FName" v-model="fName"  maxlength="60" autogrow   :dense="dense" />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 1" v-model="line_1"  maxlength="60" autogrow  :dense="dense" />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 2" v-model="line_2"  maxlength="60" autogrow  :dense="dense" />
                               <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="City" v-model="city"  maxlength="60" autogrow  :dense="dense" />
                               <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Longitude " v-model="longitude"  maxlength="60" autogrow  :dense="dense" />
                               <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Latitude" v-model="latitude"  maxlength="60" autogrow  :dense="dense" />
@@ -94,7 +94,7 @@
                       
                       <q-card-actions align="right" class="text-primary">
                          <div class="col-6 q-px-xl q-pt-lg q-pb-md items-center"> 
-                               <q-btn class= "btnStyle" type="submit" flat label="Save" v-close-popup />
+                               <q-btn class= "btnStyle" type="submit"  @click="createCenter(fName, line_1, line_2,city, latitude, longitude, pNo,email)" label="Save" v-close-popup />
                           </div>
 
                           <div class="col-6 q-px-xl q-pt-lg q-pb-md items-center">
@@ -113,21 +113,21 @@
                         <p class = "q-px-xl q-pt-lg q-pb-md textStyle"> Edit Topic </p>
                       </div>
                       <div class= "col-2 q-pa-sm">
-                        <q-btn flat icon="fa-solid fa-x" v-close-popup />
+                        <q-btn flat icon="fa-solid fa-x" v-close-popup @click ="clearData"/>
                       </div>
                     </div>
 
                      <div class="row">
                         <div class= "col-12 text-p q-px-xl textContent">
-                            <q-form @submit="createReliefCentre"> 
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="FName" v-model="Name"  maxlength="60" autogrow   :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 1" v-model="addressLone"  maxlength="60" autogrow  :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 2" v-model="addressLtwo"  maxlength="60" autogrow  :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="City" v-model="city"  maxlength="60" autogrow  :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Longitude " v-model="longitude"  maxlength="60" autogrow  :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Latitude" v-model="latitude"  maxlength="60" autogrow  :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Phone Number" v-model="pNo"  maxlength="60" autogrow   :dense="dense" />
-                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Email Address" v-model="email" type="email" maxlength="60" autogrow   :dense="dense" />
+                            <q-form> 
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="FName" v-model="fName"  maxlength="60" autogrow :v-text="current_row.city"  />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 1" v-model="line_1"  maxlength="60" autogrow  />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Address Line 2" v-model="line_2"  maxlength="60" autogrow   />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="City" v-model="city"  maxlength="60" autogrow   />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Longitude " v-model="longitude"  maxlength="60" autogrow  />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Latitude" v-model="latitude"  maxlength="60" autogrow   />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Phone Number" v-model="pNo"  maxlength="60" autogrow    />
+                              <q-input class="q-py-sm" outlined style="max-width : 90%" placeholder="Email Address" v-model="email" type="email" maxlength="60" autogrow    />
                             </q-form>
                       </div>
                      </div>                  
@@ -136,11 +136,11 @@
                       </template>
                       <q-card-actions align="right" class="text-primary">
                          <div class="col-6 q-px-xl q-pt-lg q-pb-md items-center"> 
-                               <q-btn class= "btnStyle" flat label="Save" type="submit" v-close-popup />
+                               <q-btn class= "btnStyle" flat label="Save" type="submit" @click ="editCenter(current_row.id, fName, line_1, line_2,city, latitude, longitude, pNo,email)" v-close-popup />
                           </div>
 
                           <div class="col-6 q-px-xl q-pt-lg q-pb-md items-center">
-                              <q-btn class= "btnStyle2" flat label="Cancel" v-close-popup />
+                              <q-btn class= "btnStyle2" flat label="Cancel" @click ="clearData" v-close-popup />
                           </div>
                       </q-card-actions>
                   </q-card>
@@ -165,7 +165,7 @@
                       
                       <q-card-actions align="right" class="text-primary">
                          <div class="col-6 q-px-xl q-pt-lg q-pb-md items-center"> 
-                               <q-btn class= "btnStyle" flat label="Confirm" @click = "deleteTopic(current_row)" v-close-popup/>
+                               <q-btn class= "btnStyle" flat label="Confirm" @click = "deleteCenter(current_row.id)" v-close-popup/>
                           </div>
 
                           <div class="col-6 q-px-xl q-pt-lg q-pb-md items-center">
@@ -191,8 +191,19 @@ export default defineComponent({
 
 
   setup(props) {
-    const value = Date.now()
-    const topics = ref([])
+    const commonURL = 'https://swarmnet.sundaebytes.com/api/common'
+    const baseAdminURL = 'https://swarmnet.sundaebytes.com/api/admin'
+
+    const fName = ref('')
+    const city = ref('')
+    const line_1 = ref('')
+    const line_2 = ref('')
+    const latitude = ref('')
+    const longitude = ref('')
+    const pNo = ref('')
+    const email = ref('')
+
+    const centers = ref([])
     const data = ref(null)
     const columns = [{
         name:'name',
@@ -231,34 +242,46 @@ export default defineComponent({
         email:'kwasiedwards@gmail.com',
     }]
 
-    function getTopics(){
-        let url = "https://swarmnet.sundaebytes.com/api/topics"
+    function getReliefCenter(){
+        let url = commonURL+'/relief_center'
         api.get(url, {
           method: 'GET',
           headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
+          'Access-Control-Allow-Origin': '*',
+          Authorization:  'Bearer '+ localStorage.getItem('token'),
+          }
         }).then((response) => {
-            data.value =  response.data.topics
-            console.log(data.value)
+            data.value =  response.data.centers
             for(let i of data.value){
-              topics.value.push({ id: i.id,
-                topic: i.text,
-                createdby:'Kwasi Edwards',
-                createddate:'07.22.22',
-                seeAllPostByTopic:'100',
+              centers.value.push({ 
+                id:i.id,
+                name: i.first_name,
+                city: i.city,
+                email:i.email,
+                latitude:i.latitude,
+                longitude:i.longitude,
+                phone:i.phone,
+                profile_image: i.profile_image,
+                subscriptions:i.subscriptions,
+                line_1: i.line_1,
+                line_2: i.line_2,
             })
         }})
     }
 
 
-    function createTopic(newTopic){
-      console.log(newTopic)
-      
-      let url = "https://swarmnet.sundaebytes.com/api/admin/topics"
+    function createCenter(fname, line_1, line_2,city, lat, lon, pNo,email){
+      console.log('Hello there')
+      let url = baseAdminURL+'/users/relief_center'
       api.post(url, {
-        text:newTopic,
-        level: 1
+        name:fname,
+        line_1:line_1,
+        line_2:line_2,
+        city:city,
+        longitude:lon,
+        latitude:lat,
+        phone:pNo,
+        email:email
       }, {
         headers: {
            Authorization:  'Bearer '+ localStorage.getItem('token') ,
@@ -266,24 +289,33 @@ export default defineComponent({
           'Content-Type': 'application/json',
         }
       }).then((response) => {
-        data.value = response.data.topic
-        console.log(data.value)
-        topics.value.push({
-                id: data.value.id,
-                topic: data.value.text,
-                createdby:'Kwasi Edwards',
-                createddate:'07.22.22',
-                seeAllPostByTopic:'100',
+        data.value = response.data.center
+        centers.value.push({
+                 id:data.id,
+                name: data.value.first_name,
+                city: data.value.city,
+                email:data.value.email,
+                latitude:data.value.longitude,
+                phone:data.value.phone,
+                profile_image: data.value.profile_image,
+                subscriptions:data.value.subscriptions,
+                line_1: data.value.line_1,
+                line_2: data.value.line_2,
         })
       })
     }
 
-    function editTopic(newTopic,currentTopic){
-  
-      let url = "https://swarmnet.sundaebytes.com/api/admin/topic/"+currentTopic.id
+    function editCenter(id,fname, line_1, line_2,city, lat, lon, pNo,email){
+      let url = baseAdminURL+'/users/relief_center/'+id
       api.put(url, {
-        text:newTopic,
-        level: 1
+       name:fname,
+        line_1:line_1,
+        line_2:line_2,
+        city:city,
+        longitude:lon,
+        latitude:lat,
+        phone:pNo,
+        email:email
       }, {
         headers: {
            Authorization:  'Bearer '+ localStorage.getItem('token') ,
@@ -292,13 +324,13 @@ export default defineComponent({
         }
       }).then((response) => {
         console.log(response)
-        topics.value = ([])
-        getTopics()
+        centers.value = ([])
+        getReliefCenter()
       })
     }
 
-    function deleteTopic(currentTopic){
-      let url = "https://swarmnet.sundaebytes.com/api/admin/topic/"+currentTopic.id
+    function deleteCenter(id){
+      let url = baseAdminURL+'/users/relief_center/'+id
       api.delete(url, {
         headers: {
            Authorization:  'Bearer '+ localStorage.getItem('token') ,
@@ -306,37 +338,58 @@ export default defineComponent({
         }
       }).then((response) => {
         console.log(response)
-        topics.value = ([])
-        getTopics()
+        centers.value = ([])
+        getReliefCenter()
       })
     }
 
-    function datePassed() {
-    //  console.log(Date.parse(props.data.created))
-    //  console.log(formatDistance(Date.parse(props.data.created), new Date(), { addSuffix: true }))
-        let returnDate = new Date(Date.parse(props.data.created))
-      return  returnDate.toLocaleString()
-       // return formatDistance(Date.parse(props.data.created), new Date(), { addSuffix: true })
-    }
   onMounted(() => {
-    getTopics();
+    getReliefCenter();
   })
     return{
-      datePassed,
+      fName,
+      city,
+      line_1,
+      line_2,
+      latitude,
+      longitude,
+      pNo,
+      email,
       data,
+      dense:ref(false),
       prompt: ref(false),
       filter: ref(''),
       promptEdit: ref(false),
       promptDelete: ref(false),
       current_row:ref(''),
       search:ref(''),
-      getTopics,
-      editTopic,
-      createTopic,
-      deleteTopic,
-      topics,
+      getReliefCenter,
+      editCenter,
+      createCenter,
+      deleteCenter,
+      centers,
       columns,
       rows,
+      loadData(current_row) {
+        fName.value = current_row.name,
+        city.value = current_row.city,
+        line_1.value = current_row.line_1,
+        line_2.value = current_row.line_2,
+        latitude.value = current_row.latitude,
+        longitude.value = current_row.longitude,
+        pNo.value = current_row.phone,
+        email.value = current_row.email
+      },
+      clearData(){
+        fName.value = '',
+        city.value = '',
+        line_1.value = '',
+        line_2.value = '',
+        latitude.value = '',
+        longitude.value = '',
+        pNo.value = '',
+        email.value = ''
+      },
     }
   }
 })
